f3505472b9ccabfcb18ccd840ab8c8f0f88c5660
Norhan308 <norhan.30.8@gmail.com>
2024-01-18 16:05:24 +0200

    Create fakeData file

---
+++ public/fakeData.ts
@@ -0,0 +1,84 @@
+export const mentors = [
+  {
+    id: "333333333",
+    email: "mentor1@gmail.com",
+    username: "Mentor mentor 1",
+    password: "mentor1",
+    field: ["Data analyst"],
+    dateCreated: "2024-09-07T08:37:37Z",
+    deleted: false,
+    imgUrl: "https://picsum.photos/seed/picsum/200/300",
+    studentsList: ["555555555", "666666666"],
+  },
+  {
+    id: "444444444",
+    email: "mentor2@gmail.com",
+    username: "Mentor mentor 2",
+    password: "mentor2",
+    field: ["Data analyst"],
+    dateCreated: "2024-09-07T08:37:37Z",
+    deleted: false,
+    imgUrl: "https://picsum.photos/seed/picsum/200/300",
+    studentsList: ["777777777", "888888888"],
+  },
+];
+
+export const students = [
+  {
+    id: "555555555",
+    email: "student1@gmail.com",
+    username: "Student student 1",
+    password: "student1",
+    dateCreated: "2024-09-07T08:37:37Z",
+    deleted: false,
+    imgUrl: "https://picsum.photos/seed/picsum/200/300",
+    description: "Software Engineer student, searching for DevOps",
+  },
+  {
+    id: "666666666",
+    email: "student2@gmail.com",
+    username: "Student student 2",
+    password: "student2",
+    dateCreated: "2024-09-07T08:37:37Z",
+    deleted: false,
+    imgUrl: "https://picsum.photos/seed/picsum/200/300",
+    description: "Software Engineer student, searching for DevOps",
+  },
+  {
+    id: "777777777",
+    email: "student3@gmail.com",
+    username: "Student student 3",
+    password: "student3",
+    dateCreated: "2024-09-07T08:37:37Z",
+    deleted: false,
+    imgUrl: "https://picsum.photos/seed/picsum/200/300",
+    description: "Software Engineer student, searching for DevOps",
+  },
+  {
+    id: "888888888",
+    email: "student4@gmail.com",
+    username: "Student student 4",
+    password: "student4",
+    dateCreated: "2024-09-07T08:37:37Z",
+    deleted: false,
+    imgUrl: "https://picsum.photos/seed/picsum/200/300",
+    description: "Software Engineer student, searching for DevOps",
+  },
+];
+
+export const messages = [
+  {
+    id: "999999999",
+    dateCreated: "2024-09-07T08:37:37Z",
+    text: "Hi, how are you?",
+    from: "555555555",
+    to: "444444444",
+  },
+  {
+    id: "111111112",
+    dateCreated: "2024-09-07T08:37:37Z",
+    text: "Fine :)",
+    from: "444444444",
+    to: "555555555",
+  },
+];
